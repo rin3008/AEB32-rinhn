12039d3e35d99ad2feb6e2313dad4d13a3b3ae54
Rin <rin.ho.vcr@gmail.com>
2022-12-12 18:19:44 +0700

    homework

---
+++ homework/session6/demo.js
@@ -0,0 +1,25 @@
+let num = 1
+let age = 2
+let how = 3
+console.log('number', num);
+console.log('tuoi', age);
+console.log('thenao', how);
+let boo = true
+console.log("boolean", boo);
+let und
+let unde = undefined
+console.log("undefined", und, unde)
+let nu = null
+console.log("null", nu);
+let user = {
+    name: 'ten user',
+    age: 'tuoi user'
+}
+console.log("object", user);
+
+console.log("object.name", user.name);
+
+let ar = [1, 2, 3, 4]
+let ar1 = ['a', 'b', 'c']
+console.log("array<number>", ar);
+console.log("lay phan tu", ar[0]);
